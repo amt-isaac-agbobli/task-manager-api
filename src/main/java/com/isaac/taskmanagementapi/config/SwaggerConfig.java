92fd0b7ca7b632aaba69cb22bc406ae4755621c7
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-06 09:33:00 +0000

    Add Swagger security scheme and update API description

--- src/main/java/com/isaac/taskmanagementapi/config/SwaggerConfig.java
+++ src/main/java/com/isaac/taskmanagementapi/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.isaac.taskmanagementapi.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info().title("Task Manager API").version("1.0"));
-    }
-
+        return new OpenAPI()
+                .info(new Info().title("Task Manager API")
+                        .description("A simple task management API")
+                        .version("1.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")));
+}
 }
