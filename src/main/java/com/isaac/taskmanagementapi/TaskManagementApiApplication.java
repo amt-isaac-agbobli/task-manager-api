e769692ded424a592aebb823a2ca6df82a766056
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-06 07:44:32 +0000

    Add Docker configuration and initial project files

---
+++ src/main/java/com/isaac/taskmanagementapi/TaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.taskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaac/taskmanagementapi/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.taskmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
