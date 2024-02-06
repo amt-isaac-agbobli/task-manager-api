6fbf31d11919e1f219e5e7fc7495b66350131854
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-06 07:55:47 +0000

    Add Springdoc OpenAPI dependency and configure SwaggerConfig

--- src/main/java/com/isaac/taskmanagementapi/config/SwaggerConfig.java
+++ src/main/java/com/isaac/taskmanagementapi/config/SwaggerConfig.java
@@ -1,2 +1,16 @@
-package com.isaac.taskmanagementapi.config;public class SwaggerConfig {
+package com.isaac.taskmanagementapi.config;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Task Manager API").version("1.0"));
+    }
+
 }
