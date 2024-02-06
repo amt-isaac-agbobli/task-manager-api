4de78e7320d0d1f01eb1bfcc15912bb60fb5504d
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-06 16:35:13 +0000

    Add password and user service interfaces***

---
+++ src/main/java/com/isaac/taskmanagementapi/service/password/interfaces/IResetPasswordService.java
@@ -0,0 +1,2 @@
+package com.isaac.taskmanagementapi.service.password.interfaces;public interface IResetPasswordService {
+}
---
+++ src/main/java/com/isaac/taskmanagementapi/service/user/interfaces/IUserService.java
@@ -0,0 +1,2 @@
+package com.isaac.taskmanagementapi.service.user.interfaces;public interface IUserService {
+}
