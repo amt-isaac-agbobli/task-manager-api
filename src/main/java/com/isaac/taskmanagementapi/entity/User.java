b3f41783fb160345d7c0befb974dd8760d167264
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-06 08:14:53 +0000

    Add validation annotations to User entity class

--- src/main/java/com/isaac/taskmanagementapi/entity/User.java
+++ src/main/java/com/isaac/taskmanagementapi/entity/User.java
@@ -1,2 +1,29 @@
-package com.isaac.taskmanagementapi.entity;public class User {
+package com.isaac.taskmanagementapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    int id;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    String email;
+
+    @NotBlank(message = "Password is required")
+    @Length(min = 6, message = "Password must be at least 6 characters")
+    String password;
 }
