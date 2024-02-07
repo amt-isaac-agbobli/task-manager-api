31027aa3993fe39efa0f55cca9a0a335494aaa5d
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-07 10:07:15 +0000

    Add findByCreatedBy method to TaskRepository and create GetTaskService class

--- src/main/java/com/isaac/taskmanagementapi/repository/TaskRepository.java
+++ src/main/java/com/isaac/taskmanagementapi/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package com.isaac.taskmanagementapi.repository;
 
 import com.isaac.taskmanagementapi.entity.Task;
+import com.isaac.taskmanagementapi.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository  extends JpaRepository<Task, Integer> {
+    Page<Task> findByCreatedBy(User user, Pageable pageable);
 }
--- src/main/java/com/isaac/taskmanagementapi/service/task/GetTaskService.java
+++ src/main/java/com/isaac/taskmanagementapi/service/task/GetTaskService.java
@@ -1,2 +1,20 @@
-package com.isaac.taskmanagementapi.service.task;public class GetTaskService {
+package com.isaac.taskmanagementapi.service.task;
+
+import com.isaac.taskmanagementapi.repository.TaskRepository;
+import com.isaac.taskmanagementapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetTaskService {
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+    @Autowired
+    public GetTaskService(TaskRepository taskRepository,
+                          UserRepository userRepository) {
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+    }
+
+
 }
