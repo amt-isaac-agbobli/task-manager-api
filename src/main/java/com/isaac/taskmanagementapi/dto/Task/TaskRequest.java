5dad9322e63933b6c73d5f1a0fa4af6cd16b4cce
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-07 08:01:40 +0000

    Add validation annotations to TaskRequest class

--- src/main/java/com/isaac/taskmanagementapi/dto/Task/TaskRequest.java
+++ src/main/java/com/isaac/taskmanagementapi/dto/Task/TaskRequest.java
@@ -1,2 +1,27 @@
-package com.isaac.taskmanagementapi.dto.Task;public class TaskRequest {
+package com.isaac.taskmanagementapi.dto.Task;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskRequest {
+    @NotBlank(message = "Title is required")
+    @Length(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Length(min = 10, max = 1000, message = "Description must be between 3 and 1000 characters")
+    private String description;
+
+    @NotBlank(message = "Due date is required")
+    private String dueDate;
+
+    private int assignedTo;
 }
