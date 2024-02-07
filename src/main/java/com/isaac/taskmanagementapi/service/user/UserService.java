448e1eb580c80d59f37695edc04f6e877d183169
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-07 11:32:05 +0000

    Add ehcache configuration and dependencies, enable caching

--- src/main/java/com/isaac/taskmanagementapi/service/user/UserService.java
+++ src/main/java/com/isaac/taskmanagementapi/service/user/UserService.java
@@ -1,33 +1,28 @@
 package com.isaac.taskmanagementapi.service.user;
-
-import com.isaac.taskmanagementapi.dto.password.UpdatePasswordRequest;
 import com.isaac.taskmanagementapi.entity.User;
 import com.isaac.taskmanagementapi.exception.HttpException;
 import com.isaac.taskmanagementapi.repository.UserRepository;
 import com.isaac.taskmanagementapi.service.user.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
 
 @Service
 public class UserService implements IUserService {
     private final UserRepository userRepository;
 
-    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
+    @Cacheable(value = "userProfile", key = "#email")
     public User getUserProfile(String email) {
         User user = userRepository.findByEmail(email);
         if (user == null) {
@@ -36,11 +31,5 @@ public class UserService implements IUserService {
         user.setPassword(null);
         return user;
     }
-
-
-
-
-
-
 
 }
