3a62f8b01c613f6f8bc033a1f8f6409ffb4926e9
amt-isaac-agbobli <isaac.agbobli@amalitech.org>
2024-02-07 08:39:16 +0000

    Add exception handling for assigning tasks to non-existent users

--- src/main/java/com/isaac/taskmanagementapi/service/task/AddTaskService.java
+++ src/main/java/com/isaac/taskmanagementapi/service/task/AddTaskService.java
@@ -4,7 +4,10 @@ import com.isaac.taskmanagementapi.dto.Task.AddTaskRequest;
 import com.isaac.taskmanagementapi.entity.Task;
 import com.isaac.taskmanagementapi.entity.User;
 import com.isaac.taskmanagementapi.enums.Status;
+import com.isaac.taskmanagementapi.exception.HttpException;
 import com.isaac.taskmanagementapi.repository.TaskRepository;
+import com.isaac.taskmanagementapi.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,13 +16,17 @@ import java.util.Map;
 @Service
 public class AddTaskService {
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
 
-    public AddTaskService(TaskRepository taskRepository) {
+    public AddTaskService(TaskRepository taskRepository, UserRepository userRepository) {
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
     }
 
     public Object addTask(AddTaskRequest request, User user) {
         int assignedTo = request.getAssignedTo() == 0 ? user.getId() : request.getAssignedTo();
+
+        assignee(assignedTo);
 
         Task task = Task.builder()
                      .title(request.getTitle())
@@ -32,5 +39,11 @@ public class AddTaskService {
                 taskRepository.save(task);
 
         return Map.of( "message", "Task created successfully");
+    }
+
+    private void assignee(int assignedTo) {
+        userRepository.findById(assignedTo)
+                .orElseThrow(() -> new HttpException("User you are trying to assign the task to does not exist",
+                        HttpStatus.NOT_FOUND));
     }
 }
